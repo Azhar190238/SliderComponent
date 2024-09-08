1d587c8291d0bada32f02ee86e9ab5d821ac42ad
MD AZHARUDDIN <azhar73397@gmail.com>
2024-09-08 16:08:35 +0600

    Alhamdulilah Updated

--- app/src/components/AboutSlider.jsx
+++ app/src/components/AboutSlider.jsx
@@ -88,7 +88,7 @@ const AboutSlider = () => {
                         }}
                         className={`rounded-[4px] lg:w-[31.6%] md:w-[48%] w-[100%]  flex items-center h-80 bg-top bg-no-repeat bg-cover  flex-shrink-0`}
                     >
-                        {/* Apply group here to allow hover effects to propagate to children */}
+                  
                         <div className="w-full mx-[20px] rounded-[4px] bg-gradient-to-b relative top-28 duration-300 border-[1px] border-[#E67529] backdrop-blur-[5px] ease-in-out bg-[#FFFFFF1A] hover:bg-[#E67529] group">
                             <h1 className="text-white text-5xl flex uppercase justify-center py-1 pl-2 font-extrabold lato group-hover:text-white">
                                 {banner.title}
